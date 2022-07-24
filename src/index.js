d98763a8f9b45935b122c94557254a929598da16
Maria <106482989+DearrMaria@users.noreply.github.com>
2022-07-24 19:17:12 +0200

    feat: update the solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackes = {};
+  for (let item of bracketsConfig) {
+    brackes[item[0]] = item[1];
+  }
+
+  let sqware = [];
+  for (let i = 0; i < str.length; i++) {
+    let lastItem = sqware[sqware.length-1];
+    let current = str[i];
+    
+    if (current in brackes) {
+      (current == brackes[current]) && (current == lastItem) ? 
+      sqware.pop() : sqware.push(current);
+    } else {
+      if (sqware.length === 0) {
+        return false;
+      }
+      brackes[lastItem] == current ? sqware.pop() : false;
+    }
+  } 
+    return sqware.length === 0;
 }
